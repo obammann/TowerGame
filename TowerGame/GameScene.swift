a16196b42d7c609ab35a7f9431837e31a1b224d5
Mike Hüsing <mikinho90@gmx.de>
2016-06-14 14:27:51 +0200

    merged our stuff and created JoystickEntity, and outsourced the joystick

--- TowerGame/GameScene.swift
+++ TowerGame/GameScene.swift
@@ -10,13 +10,14 @@ import SpriteKit
 
 class GameScene: SKScene {
     
-    var playerWalkingFrames : [SKTexture]!
+    
     var entityManager: EntityManager!
     var player: PlayerEntity!
     var playerNode: SKSpriteNode!
     var joystick = AnalogJoystick(diameters: (1 , 1))
     var cam = SKCameraNode()
     var camOldX: CGFloat = 0.0
+    var joystickEntity: JoystickEntity!
     
     // Update time
     var lastUpdateTimeInterval: NSTimeInterval = 0
@@ -28,6 +29,17 @@ class GameScene: SKScene {
 
         playerNode = (self.childNodeWithName("player") as? SKSpriteNode)!
         player = PlayerEntity(node: playerNode, scene: self, maxHealth: 10)
+        
+        
+        joystickEntity = JoystickEntity(joystick: joystick, scene: self)
+        
+        cam = self.childNodeWithName("playerCamera") as! SKCameraNode
+        cam.position = playerNode.position
+        camOldX = cam.position.x
+        
+
+        
+
         
         entityManager.addEntityFromEditor(player)
         let position = CGPoint(x: 50, y: 50)
@@ -44,38 +56,13 @@ class GameScene: SKScene {
 //        entityManager.addEntityFromEditor(bullet)
         
         
-        initJoystick()
-        
-        cam = self.childNodeWithName("playerCamera") as! SKCameraNode
-        cam.position = spriteNode.position
-        camOldX = cam.position.x
-        
-        let playerAnimatedAtlas = SKTextureAtlas(named: "player")
-        var walkFrames = [SKTexture]()
-        
-        let numImages = playerAnimatedAtlas.textureNames.count
-        for var i=0; i<numImages; i+=1 {
-            let bearTextureName = "character\(i)"
-            walkFrames.append(playerAnimatedAtlas.textureNamed(bearTextureName))
-        }
-        playerWalkingFrames = walkFrames
-        let firstFrame = playerWalkingFrames[0]
+
        
 
     }
-    func playerMoveEnded() {
-        playerNode.removeAllActions()
-    }
+
     
-    func walkingPlayer() {
-        //This is our general runAction method to make our bear walk.
-        playerNode.runAction(SKAction.repeatActionForever(
-            SKAction.animateWithTextures(playerWalkingFrames,
-                timePerFrame: 0.12,
-                resize: false,
-                restore: true)),
-                       withKey:"walkingInPlacePlayer")
-    }
+
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
@@ -89,53 +76,12 @@ class GameScene: SKScene {
         lastUpdateTimeInterval = currentTime
         
         entityManager.update(deltaTime)
-        cam.position.x = spriteNode.position.x
+        cam.position.x = playerNode.position.x
         joystick.position.x += cam.position.x - camOldX
         camOldX = cam.position.x
         
     }
     
     
-    func initJoystick() {
-        
-        joystick.stick.diameter = size.width
-        joystick.substrate.diameter = size.width
-        joystick.stick.color = UIColor.whiteColor()
-        joystick.substrate.color = UIColor.blueColor()
-        joystick.stick.alpha = 0.01
-        joystick.substrate.alpha = 0.01
-        joystick.position = CGPoint(x:-133, y:50)
-        joystick.zPosition = 10
-        addChild(joystick)
-        
-        joystick.startHandler = { [unowned self] location in
-            
-            self.walkingPlayer()
-            self.joystick.stick.diameter = 100
-            self.joystick.substrate.diameter = 150
-            self.joystick.position = location
-            self.joystick.stick.alpha = 1
-            self.joystick.substrate.alpha = 1
-            self.joystick.stick.color = UIColor.whiteColor()
-            self.joystick.substrate.color = UIColor.blueColor()
-        }
-        
-        
-        joystick.trackingHandler = { [unowned self] data in
-            
-            let aN = self.spriteNode
-            aN.position = CGPointMake(aN.position.x + (data.velocity.x * 0.15), aN.position.y + (data.velocity.y * 0.15))
-            aN.zRotation = data.angular
-        }
-        
-        joystick.stopHandler = { [unowned self] in
-            self.joystick.stick.diameter = self.size.width
-            self.joystick.substrate.diameter = self.size.width
-            self.joystick.zPosition = 10
-            self.joystick.stick.alpha = 0.01
-            self.joystick.substrate.alpha = 0.01
-            self.joystick.position = CGPoint(x:self.cam.position.x - 79, y:-self.size.height * 0.5)
-            self.playerMoveEnded()
-        }
-    }
+
 }
---
+++ TowerGame/JoystickEntity.swift
@@ -0,0 +1,67 @@
+//
+//  JoystickEntity.swift
+//  TowerGame
+//
+//  Created by Mike Hüsing on 14.06.16.
+//  Copyright © 2016 Oliver Bammann. All rights reserved.
+//
+
+import Foundation
+import GameKit
+
+
+class JoystickEntity: GKEntity {
+    
+    
+    var joystick: AnalogJoystick
+    var scene: GameScene
+    
+    
+    
+    init(joystick: AnalogJoystick, scene: GameScene) {
+        self.scene = scene
+        self.joystick = joystick
+        super.init()
+        joystick.stick.diameter = scene.size.width
+        joystick.substrate.diameter = scene.size.width
+        joystick.stick.color = UIColor.whiteColor()
+        joystick.substrate.color = UIColor.blueColor()
+        joystick.stick.alpha = 0.01
+        joystick.substrate.alpha = 0.01
+        joystick.position = CGPoint(x:-133, y:50)
+        joystick.zPosition = 10
+        scene.addChild(joystick)
+        
+        self.joystick.startHandler = { [unowned self] location in
+            
+            self.scene.player.playerWalk()
+            self.joystick.stick.diameter = 100
+            self.joystick.substrate.diameter = 150
+            self.joystick.position = location
+            self.joystick.stick.alpha = 1
+            self.joystick.substrate.alpha = 1
+            self.joystick.stick.color = UIColor.whiteColor()
+            self.joystick.substrate.color = UIColor.blueColor()
+        }
+        
+        
+        self.joystick.trackingHandler = { [unowned self] data in
+            
+            let aN = scene.playerNode
+            aN.position = CGPointMake(aN.position.x + (data.velocity.x * 0.15), aN.position.y + (data.velocity.y * 0.15))
+            aN.zRotation = data.angular
+        }
+        
+        self.joystick.stopHandler = { [unowned self] in
+            self.joystick.stick.diameter = scene.size.width
+            self.joystick.substrate.diameter = scene.size.width
+            self.joystick.zPosition = 10
+            self.joystick.stick.alpha = 0.01
+            self.joystick.substrate.alpha = 0.01
+            self.joystick.position = CGPoint(x:scene.cam.position.x - 79, y:-scene.size.height * 0.5)
+            self.scene.player.playerEndAnimations()
+        }
+    }
+    
+
+}
