4be1b75c75938fbf753011ae7c7711b6cb4d6742
Mike Hüsing <mikinho90@gmx.de>
2016-06-21 01:09:55 +0200

    smoke and resizing car

--- TowerGame/PlayerEntity.swift
+++ TowerGame/PlayerEntity.swift
@@ -14,11 +14,54 @@ class PlayerEntity: GKEntity {
 
     var node: SKSpriteNode
     var scene: GameScene
+    var movementFrames: [SKTexture]!
+    let group: SKAction
     
     init(node: SKSpriteNode, scene: GameScene, maxHealth: Int) {
         
         self.scene = scene
         self.node = node
+        
+        /*var actions = Array<SKAction>()
+         
+         actions.append(SKAction.sequence([animationWait, animationScale]))
+         actions.append(SKAction.sequence([animationWait, animationAlpha]))
+         
+         let group = SKAction.group(actions)
+         
+         node.runAction(group)*/
+        
+        /*self.node.runAction(SKAction.repeatActionForever(
+         SKAction.animateWithTextures(self.movementFrames,
+         timePerFrame: 0.08,
+         resize: false,
+         restore: true)),
+         withKey:"movement")*/
+        
+        let scaleAction = SKAction.scaleTo(1, duration: 5)
+
+        
+        let playerAnimatedAtlas = SKTextureAtlas(named: "puff")
+        var walkFrames = [SKTexture]()
+        
+        let numberImages = playerAnimatedAtlas.textureNames.count
+        for i in 0 ..< numberImages {
+            let textureName = "whitePuff\(i)"
+            walkFrames.append(playerAnimatedAtlas.textureNamed(textureName))
+        }
+        movementFrames = walkFrames
+        
+        let textureAction = SKAction.animateWithTextures(self.movementFrames,
+                timePerFrame: 0.2,
+                resize: false,
+                restore: true)
+        
+        var actions = Array<SKAction>()
+        
+        actions.append(scaleAction)
+        actions.append(textureAction)
+        
+        group = SKAction.group(actions)
         
         super.init()
         
@@ -40,6 +83,17 @@ class PlayerEntity: GKEntity {
         
         let shieldComponent = ShieldComponent(node: self.node, maxShieldCapacity: 10, scene: self.scene)
         addComponent(shieldComponent)
+        
+        
+        
+    }
+    func createGas() {
+        let gas = SKSpriteNode(imageNamed: "whitePuff0")
+        gas.setScale(0.2)
+        gas.position = scene.playerNode.position
+        scene.addChild(gas)
+        print(gas)
+        gas.runAction(SKAction.sequence([group, SKAction.removeFromParent()]))
     }
     
     func playerWalk() {
